b585eccddee8fb016a46b5d97519b6f77b3a8486
Hansana Ranaweera <73303406+ranaHP@users.noreply.github.com>
2022-07-28 00:12:59 +0530

    This commit for adding routes

--- App.js
+++ App.js
@@ -7,33 +7,70 @@ import {
   Box,
   extendTheme,
   ScrollView,
-  StatusBar,
+  View,
 } from 'native-base';
 
 import {MauthurulaTheme} from './src/theme/base/index';
 import ArticleCard from './src/components/ArticleCard';
 import Icon from 'react-native-vector-icons/Ionicons';
-
+import {
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert, Platform,
+  StatusBar, Dimensions,
+} from "react-native";
 
 const theme = extendTheme(v3CompatibleTheme, MauthurulaTheme);
 
 const App: () => Node = () => {
+  console.log(Dimensions.get("window"));
   return (
     <NativeBaseProvider theme={theme}>
-            <StatusBar></StatusBar>
-      <ScrollView >
-      <Text fontSize="xl" alignItems="center" textAlign="center">මව් තුරුල </Text>
-        <Box bg="#dddddd" alignItems="center" justifyContent="space-evenly" safeArea>
-        <Icon name="md-menu" size={30} color="#900" />
-
-          <ArticleCard />
-          <ArticleCard />
-          <ArticleCard />
-        </Box>
-        </ScrollView>
+      <SafeAreaView style={style.container}>
+        <TouchableHighlight
+          onPress={() => {
+            console.log('Clicked');
+          }}>
+          <Image
+            blurRadius={1}
+            fadeDuration={10000}
+            source={{
+              uri: 'https://picsum.photos/200/300',
+              width: 200,
+              height: 300,
+            }}
+          />
+        </TouchableHighlight>
+        <Button
+          onPress={() => {
+            Alert.prompt('Hansana Ranaweera', 'My name', text => {
+              console.log(text);
+            });
+          }}
+          title={' Click'}
+        />
+        <Text numberOfLines={1}>
+          Hello ReactHello ReactHello ReactHello ReactHello ReactHello
+          ReactHello ReactHello ReactHello ReactHello ReactHello ReactHello
+          React
+        </Text>
+      </SafeAreaView>
     </NativeBaseProvider>
   );
 };
 
-
+const style = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100%',
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+});
 export default App;
