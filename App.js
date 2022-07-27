84673d691ae1df4ef5b21b2b54ff3ad605a97b36
Hansana Ranaweera <73303406+ranaHP@users.noreply.github.com>
2022-07-27 19:57:32 +0530

    This commit for add status bar

--- App.js
+++ App.js
@@ -14,26 +14,35 @@ import {MauthurulaTheme} from './src/theme/base/index';
 import ArticleCard from './src/components/ArticleCard';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-
 const theme = extendTheme(v3CompatibleTheme, MauthurulaTheme);
 
 const App: () => Node = () => {
   return (
     <NativeBaseProvider theme={theme}>
-            <StatusBar></StatusBar>
-      <ScrollView >
-      <Text fontSize="xl" alignItems="center" textAlign="center">මව් තුරුල </Text>
-        <Box bg="#dddddd" alignItems="center" justifyContent="space-evenly" safeArea>
-        <Icon name="md-menu" size={30} color="#900" />
-
+      <ScrollView>
+        <Box
+          bg="#00ff33"
+          alignItems="center"
+          justifyContent="space-between"
+          padding="5px"
+          flexDirection="row">
+          <Text fontSize="xl" alignItems="center" textAlign="center">
+            මව් තුරුල{' '}
+          </Text>
+          <Icon name="md-menu" size={30} color="#900" />
+        </Box>
+        <Box
+          bg="#dddddd"
+          alignItems="center"
+          justifyContent="space-evenly"
+          safeArea>
           <ArticleCard />
           <ArticleCard />
           <ArticleCard />
         </Box>
-        </ScrollView>
+      </ScrollView>
     </NativeBaseProvider>
   );
 };
-
 
 export default App;
